402e9a44b6837d13ae7f9de947667800133dd9d7
Ngainer <50744304+Ngainer@users.noreply.github.com>
2022-04-21 10:15:29 -0400

    Update streamlit_app.py
    Added code to retrieve a fruit list from AWS S3 and display the dataframe on the web page using streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+import pandas as pd
 
+#Setup the main page display, header, and text
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -9,3 +11,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#Read fruit macros csv from AWS S3
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
